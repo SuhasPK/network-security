e1cd9bc386688542885ae5d3a04376faa11c9b05
SuhasPK <sparrowjack0682@gmail.com>
2024-08-17 15:46:19 +0530

    pushed the local dataset to mongodb atlas. connection working successfully.

--- get_data.py
+++ get_data.py
@@ -1,35 +1,63 @@
-import os 
+import os
 import sys
 import json
-
-from dotenv import load_dotenv
-
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 import certifi
 import pandas as pd
-import numpy as np
-import pymongo
-
 from network_security.exception.exception import NetworkSecurityException
 from network_security.logger.logger import logging
 
-class network_data_extraction():
+# MongoDB URI directly from test.py
+MONGO_DB_URL = "mongodb+srv://suhas:suhaspk990@cluster0.hqa5r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+# Print the MongoDB URL to verify
+print(f"Using MongoDB URL: {MONGO_DB_URL}")
+
+ca = certifi.where()
+
+class NetworkDataExtract():
     def __init__(self):
         try:
             pass
         except Exception as e:
-            raise NetworkSecurityException(e,sys)
-        
-    def csv_tojoin_converter(self):
+            raise NetworkSecurityException(e, sys)
+
+    def csv_to_json_convertor(self, file_path):
         try:
-            pass
+            data = pd.read_csv(file_path)
+            data.reset_index(drop=True, inplace=True)
+            records = list(json.loads(data.T.to_json()).values())
+            return records
         except Exception as e:
-            raise Network_Security_Exception(e,sys)
-        
-    def pushing_data_to_mongodb(self):
+            raise NetworkSecurityException(e, sys)
+
+    def pushing_data_to_mongodb(self, records, database, collection):
         try:
-            pass
+            self.database = database
+            self.collection = collection
+            self.records = records
+
+            # Connect to MongoDB
+            self.mongo_client = MongoClient(MONGO_DB_URL, server_api=ServerApi('1'))
+
+            self.database = self.mongo_client[self.database]
+
+            self.collection = self.database[self.collection]
+
+            # Insert the records into the MongoDB collection
+            self.collection.insert_many(self.records)
+
+            return len(self.records)
+
         except Exception as e:
-            raise Network_Security_Exception(e,self)
-        
-    if __name__ == '__main__':
-        pass
+            raise NetworkSecurityException(e, sys)
+
+if __name__ == '__main__':
+    FILE_PATH = "./network_data/network_data.csv"
+    DATABASE = "suhapk"
+    COLLECTION = "NetworkData"
+    networobj = NetworkDataExtract()
+    records = networobj.csv_to_json_convertor(FILE_PATH)
+    noofrecords = networobj.pushing_data_to_mongodb(records, DATABASE, COLLECTION)
+    print(f"Number of records inserted: {noofrecords}")
