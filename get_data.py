3bf22c5d874f5be6e1383e6d843c79077026e946
SuhasPK <sparrowjack0682@gmail.com>
2024-08-17 12:15:16 +0530

    initial get_data is setup.

---
+++ get_data.py
@@ -0,0 +1,35 @@
+import os 
+import sys
+import json
+
+from dotenv import load_dotenv
+
+import certifi
+import pandas as pd
+import numpy as np
+import pymongo
+
+from network_security.exception.execption import Network_Security_Exception
+from network_security.logger.logger import logging
+
+class network_data_extraction():
+    def __init__(self):
+        try:
+            pass
+        except Exception as e:
+            raise Network_Security_Exception(e,sys)
+        
+    def csv_tojoin_converter(self):
+        try:
+            pass
+        except Exception as e:
+            raise Network_Security_Exception(e,sys)
+        
+    def pushing_data_to_mongodb(self):
+        try:
+            pass
+        except Exception as e:
+            raise Network_Security_Exception(e,self)
+        
+    if __name__ == '__main__':
+        pass
